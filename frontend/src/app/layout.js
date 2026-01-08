512c40b4215ad8339465c775a2652392a4a2ff47
Samolazoff <samolazov.roman.alex@gmail.com>
2026-01-08 17:12:13 +0300

    Start Next.js

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+    variable: "--font-inter",
+    subsets: ["latin"],
+});
+
+export const metadata = {
+    title: "Rank by choice",
+    description: "The easiest way to build a true ranking",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+        <body className={`${inter.variable}`}>
+            {children}
+        </body>
+        </html>
+    );
+}
